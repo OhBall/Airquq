727075f207c7a8c046a4cecbed05697e5e309107
Xiaoyun Yang <xiaoyun.ouo@gmail.com>
2018-10-08 22:36:30 -0700

    edit listing controller & add view files

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -1,14 +1,22 @@
 class Api::ListingsController < ApplicationController
   def index
     @listings = Listings.all
+    render :index
   end
 
   def show
     @listing = Listing.find_by_id(params[:id])
+    render :show
   end
 
   private
   
   def listing_params
+    params.require(:listing)
+      .permit(:name, :host_id, :description, :price, :room_num, 
+              :bed_num, :bath_num, :air_conditioning, :heating, 
+              :essentials, :wifi, :tv, :washer, :kitchen, 
+              :free_parking, :hair_dryer, :shampoo, :address, 
+              :city, :state, :country, :zipcode, :lat, :lng)
   end 
 end
