2e1749f7485458ee098b8534ba7c77c3cf90cf2a
Xiaoyun Yang <xiaoyun.ouo@gmail.com>
2018-10-10 18:31:35 -0700

    add homepage component styling

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -8,16 +8,17 @@ class Homepage extends React.Component {
     this.props.fetchListings();
   }
 
-
   render() {
     return (
       <div className="homepage-container">
-        <h2 className="homepage-title">Explore the world</h2>
-        <h2 className="homepage-description">Book your next trip and escape the ordinary</h2>
-        <ul className="homepage-container">
-          <HomepageItems num="200+" city="Bali" description="Browse verified homes with artful style, in a city that wows from beach to land."/>
-          <HomepageItems num="330+" city="San Francisco" description="Discover Victorian flats, SoMa lofts, and more verified homes in a city where invention meets counterculture."/>
-          <HomepageItems num="270+" city="Rome" description="Experience the modern elegance of verified homes in this ancient and ever-evolving metropolis."/>
+        <div className="homepage-text">
+          <p id="homepage-title">Explore the world</p>
+          <p id="homepage-description">Book your next trip and escape the ordinary</p>
+        </div>
+        <ul className="homepage-item-container">
+          <HomepageItems img={window.images.page1} num="200+" city="Bali" description="Browse verified homes with artful style, in a city that wows from beach to land."/>
+          <HomepageItems img={window.images.page1} num="330+" city="San Francisco" description="Discover Victorian flats, SoMa lofts, and more verified homes in a city where invention meets counterculture."/>
+          <HomepageItems img={window.images.page1} num="270+" city="Rome" description="Experience the modern elegance of verified homes in this ancient and ever-evolving metropolis."/>
         </ul>
         <ListingIndex listings={ this.props.listings } />
       </div>
--- frontend/components/homepage/homepage_items.jsx
+++ frontend/components/homepage/homepage_items.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const HomepageItems = ({ num, city, description }) => {
+const HomepageItems = ({ img, num, city, description }) => {
   return(
     <div className="homepage-detail-container">
-      <div id="home-number">{num} VERIFIED HOMES</div>
-      <div id="city-name">{city}</div>
-      <div id="description">{description}</div>
+      <img src={img} />
+      <div className="item-text-container">
+        <div id="home-number">{num} VERIFIED HOMES</div>
+        <div id="city-name">{city}</div>
+        <div id="description">{description}</div>
+      </div>
     </div>
   )
 };
